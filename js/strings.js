f3680af5f78e276b8e22538ba200b678b5fcb93c
Amit Patel <amitpatel_7@msn.com>
2017-02-01 18:03:13 -0800

    Added JavaScript variables containing html strings
    This file contains HTML strings stored in JavaScript variables to be used to dynamically populate the restaurant's menu. This will help create a structure that will make it easier to update the restaurant's menu with additional dishes in the future.

---
+++ js/strings.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var HTMLdishPic = '<img src=%data% class="dish-img img-responsive">';
+
+
+
+
+var HTMLdishName = '<h4>%data%</h4>';
+var HTMLdishPrice = '<h5>%data%</h5>';
+var HTMLdishDescription = '<p>%data%</p>';
+
+var HTMLdishInfoContainer = '<div class="menu-info">'+ HTMLdishName + HTMLdishPrice + HTMLdishDescription + '</div>';
+
+var HTMLdishContainer = '<div class="row dish">' + HTMLdishPic + HTMLdishInfoContainer + '</div>';
