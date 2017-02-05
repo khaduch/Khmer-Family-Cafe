34074f300f1f5d967e9d9f122f8ce076acf3c973
Amit Patel <amitpatel_7@msn.com>
2017-02-04 17:51:35 -0800

    iterated over menu array to populate/render menu dishes

--- js/strings.js
+++ js/strings.js
@@ -27,6 +27,3 @@ var HTMLdishName = '<h4>%data%</h4>';
 var HTMLdishPrice = '<h5>%data%</h5>';
 var HTMLdishDescription = '<p>%data%</p>';
 
-var HTMLdishInfoContainer = '<div class="menu-info">'+ HTMLdishName + HTMLdishPrice + HTMLdishDescription + '</div>';
-
-var HTMLdishContainer = '<div class="row dish">' + HTMLdishPic + HTMLdishInfoContainer + '</div>';
