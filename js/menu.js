aba3a038d7f55c4e2addf59bbb0d020b9251c4eb
Amit Patel <amitpatel_7@msn.com>
2017-02-04 17:53:21 -0800

    used JSON to populate menu
    I did this to reduce the amount of html code and to also make it easier to update the menu for future dishes

---
+++ js/menu.js
@@ -0,0 +1,167 @@
+var menu = {
+	"dishes" : [
+		{
+			"name": "Beef Sticks",
+			"price": "$5",
+			"description": "Description of what's in the dish and how it's cooked.",
+			"image": "images/dishes/beef-sticks.jpg",
+			"altText": "beef-sticks"
+		},
+		{
+			"name": "Stuffed Chicken",
+			"price": "$5",
+			"description": "Description of what's in the dish and how it's cooked.",
+			"image": "images/dishes/stuffed-chicken.jpg",
+			"altText": "stuffed-chicken"
+		},
+		{
+			"name": "Lot Cha",
+			"price": "$5",
+			"description": "Description of what's in the dish and how it's cooked.",
+			"image": "images/dishes/lot-cha.jpg",
+			"altText": "lot-cha"
+		},
+		{
+			"name": "Fried Chicken",
+			"price": "$5",
+			"description": "Description of what's in the dish and how it's cooked.",
+			"image": "images/dishes/fried-chicken.jpg",
+			"altText": "fried-chicken"
+		},
+		{
+			"name": "Chicken Patty",
+			"price": "$5",
+			"description": "Description of what's in the dish and how it's cooked.",
+			"image": "images/dishes/chicken-patty.jpg",
+			"altText": "chicken-patty"
+		},
+		{
+			"name": "Papaya Salad",
+			"price": "$5",
+			"description": "Description of what's in the dish and how it's cooked.",
+			"image": "images/dishes/papaya-salad.jpg",
+			"altText": "papaya-salad"
+		},
+		{
+			"name": "Chow Mein",
+			"price": "$5",
+			"description": "Description of what's in the dish and how it's cooked.",
+			"image": "images/dishes/chow-mein.jpg",
+			"altText": "chow-mein"
+		},
+		{
+			"name": "Eggrolls",
+			"price": "$5",
+			"description": "Description of what's in the dish and how it's cooked.",
+			"image": "images/dishes/eggrolls.jpg",
+			"altText": "eggrolls"
+		},
+		{
+			"name": "Beef Pahlok",
+			"price": "$5",
+			"description": "Description of what's in the dish and how it's cooked.",
+			"image": "images/dishes/Beef-Pahlok.jpg",
+			"altText": "Beef-Pahlok"
+		},
+		{
+			"name": "Fried Rice",
+			"price": "$5",
+			"description": "Description of what's in the dish and how it's cooked.",
+			"image": "images/dishes/fried-rice.jpg",
+			"altText": "Fried-Rice"
+		},
+		{
+			"name": "Pho",
+			"price": "$5",
+			"description": "Description of what's in the dish and how it's cooked.",
+			"image": "images/dishes/pho.jpg",
+			"altText": "pho"
+		},
+		{
+			"name": "Spring Rolls",
+			"price": "$5",
+			"description": "Description of what's in the dish and how it's cooked.",
+			"image": "images/dishes/spring-rolls.jpg",
+			"altText": "spring-rolls"
+		},
+		{
+			"name": "Chicken/Beef Larb",
+			"price": "$5",
+			"description": "Description of what's in the dish and how it's cooked.",
+			"image": "images/dishes/chicken-beef-larb.jpg",
+			"altText": "chicken-beef-larb"
+		},
+		{
+			"name": "khmer shaved ice",
+			"price": "$5",
+			"description": "Description of what's in the dish and how it's cooked.",
+			"image": "images/dishes/khmer-shaved-ice.jpg",
+			"altText": "khmer-shaved-ice"
+		},
+		{
+			"name": "Shaved Ice Sundae",
+			"price": "$5",
+			"description": "Description of what's in the dish and how it's cooked.",
+			"image": "images/dishes/shaved-ice.jpg",
+			"altText": "shaved-ice"
+		},
+		{
+			"name": "Boba",
+			"price": "$5",
+			"description": "Description of what's in the dish and how it's cooked.",
+			"image": "images/dishes/boba.jpg",
+			"altText": "boba"
+		},
+		{
+			"name": "Thai Tea",
+			"price": "$5",
+			"description": "Description of what's in the dish and how it's cooked.",
+			"image": "images/dishes/drinks.jpg",
+			"altText": "drinks"
+		}
+	]
+}
+
+
+
+
+
+menu.display = function(){
+
+
+
+for (var dish in menu.dishes) {
+
+
+	var formattedPic = HTMLdishPic.replace("%data%", menu.dishes[dish].image);
+
+
+	var formattedName = HTMLdishName.replace("%data%", menu.dishes[dish].name);
+	var formattedPrice = HTMLdishPrice.replace("%data%", menu.dishes[dish].price);
+	var formattedDescription = HTMLdishDescription.replace("%data%", menu.dishes[dish].description);
+
+
+	var HTMLdishInfoContainer = '<div class="menu-info">'+ formattedName + formattedPrice + formattedDescription + '</div>';
+
+	var HTMLdishContainer = '<div class="row dish">' + formattedPic + HTMLdishInfoContainer + '</div>';
+
+
+
+		$("#main-column-1").append(HTMLdishContainer);	
+}
+
+
+
+
+};
+
+
+
+menu.display();
+
+
+
+
+
+
+
---
+++ js/strings.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var HTMLdishPic = '<img src=%data% class="dish-img img-responsive">';
+
+
+
+
+var HTMLdishName = '<h4>%data%</h4>';
+var HTMLdishPrice = '<h5>%data%</h5>';
+var HTMLdishDescription = '<p>%data%</p>';
+
