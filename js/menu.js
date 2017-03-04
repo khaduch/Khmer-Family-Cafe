47da3e15de170c6b91d05fed40923dc2e2363a6a
Amit Patel <amitpatel_7@msn.com>
2017-03-03 19:20:32 -0800

    Replaced dish descriptions & prices
    Client provided. Also replaced Thursday special via client request

--- js/menu.js
+++ js/menu.js
@@ -2,33 +2,33 @@ var menu = {
 	"column1dishes" : [
 		{
 			"id": "beef-sticks",
-			"name": "Beef Sticks",
-			"price": "$5",
-			"description": "Description of what's in the dish and how it's cooked.",
+			"name": "Beef Sticks (Quantity: 3)",
+			"price": "$7.50",
+			"description": "Traditional lemon grass seasoned beef kabobs with a side of pickled veggies.",
 			"image": "images/dishes/beef-sticks.jpg",
 			"altText": "beef-sticks"
 		},
 		{
 			"id": "stuffed-chicken",
-			"name": "Stuffed Chicken",
-			"price": "$5",
-			"description": "Description of what's in the dish and how it's cooked.",
+			"name": "Stuffed Chicken Wings (Quantity: 2)",
+			"price": "$8.50",
+			"description": "Cambodian style lemon grass seasoning and peanut stuffed chicken wing broiled to perfection.",
 			"image": "images/dishes/stuffed-chicken.jpg",
 			"altText": "stuffed-chicken"
 		},
 		{
 			"id": "lot-cha",
 			"name": "Lot Cha",
-			"price": "$5",
-			"description": "Description of what's in the dish and how it's cooked.",
+			"price": "$7.50",
+			"description": "Cambodian style stir fried pin drop noodles with beef topped with a fried egg served with a side of special house sweet fish sauce.",
 			"image": "images/dishes/lot-cha.jpg",
 			"altText": "lot-cha"
 		},
 		{
 			"id": "fried-chicken",
-			"name": "Fried Chicken",
-			"price": "$5",
-			"description": "Description of what's in the dish and how it's cooked.",
+			"name": "Fried Chicken (Quantity: 6pc)",
+			"price": "$6.00",
+			"description": "Traditional seasoned battered fried chicken. Served with Sweet Sauce.",
 			"image": "images/dishes/fried-chicken.jpg",
 			"altText": "fried-chicken"
 		},
@@ -36,15 +36,15 @@ var menu = {
 			"id": "chicken-patty",
 			"name": "Chicken Patty",
 			"price": "$5",
-			"description": "Description of what's in the dish and how it's cooked.",
+			"description": "Fried lemon grass seasoned chicken patty with side of pickled veggies.",
 			"image": "images/dishes/chicken-patty.jpg",
 			"altText": "chicken-patty"
 		},
 		{
 			"id": "papaya-salad",
 			"name": "Papaya Salad",
-			"price": "$5",
-			"description": "Description of what's in the dish and how it's cooked.",
+			"price": "$6.00",
+			"description": "Savory hot green papaya salad with our home sauce suited to your taste.",
 			"image": "images/dishes/papaya-salad.jpg",
 			"altText": "papaya-salad"
 		}
@@ -53,57 +53,57 @@ var menu = {
 	"column2dishes" : [
 		{
 			"id": "chow-mein",
-			"name": "Chow Mein",
-			"price": "$5",
-			"description": "Description of what's in the dish and how it's cooked.",
+			"name": "Combination Chow Mein",
+			"price": "$7.50",
+			"description": "Tradition stir fried noodles with combination of beef, chicken, and shrimp with your choice of spicy or no spicy.",
 			"image": "images/dishes/chow-mein.jpg",
 			"altText": "chow-mein"
 		},
 		{
 			"id": "eggrolls",
-			"name": "Eggrolls",
-			"price": "$5",
-			"description": "Description of what's in the dish and how it's cooked.",
+			"name": "Eggrolls (Quantity: 6pc)",
+			"price": "$3.00",
+			"description": "Cambodian style crispy eggroll with vermicelli noodles, veggies, pork, and house seasoning served with sweet and sour sauce.",
 			"image": "images/dishes/eggrolls.jpg",
 			"altText": "eggrolls"
 		},
 		{
-			"id": "Beef-Pahlok",
-			"name": "Beef Pahlok",
-			"price": "$5",
-			"description": "Description of what's in the dish and how it's cooked.",
-			"image": "images/dishes/Beef-Pahlok.jpg",
-			"altText": "Beef-Pahlok"
+			"id": "Pahok-Ktiss",
+			"name": "Pahok Ktiss",
+			"price": "$8.50",
+			"description": "Sweet and Spicy ground pork served with a side of fresh veggies and rice.",
+			"image": "images/dishes/Pahok-Ktiss.jpg",
+			"altText": "Pahok-Ktiss"
 		},
 		{
 			"id": "fried-rice",
 			"name": "Fried Rice",
-			"price": "$5",
-			"description": "Description of what's in the dish and how it's cooked.",
+			"price": "$7.50",
+			"description": "Cambodian style house fried rice with combination of beef, chicken, and shrimp.",
 			"image": "images/dishes/fried-rice.jpg",
 			"altText": "Fried-Rice"
 		},
 		{
 			"id": "pho",
-			"name": "Pho",
-			"price": "$5",
-			"description": "Description of what's in the dish and how it's cooked.",
+			"name": "Combo Pho 'Kathiew'",
+			"price": "$8.00",
+			"description": "Traditional broth soup with rice noodle, herb, beef, shrimp, meatballs, and crab meat.",
 			"image": "images/dishes/pho.jpg",
 			"altText": "pho"
 		},
 		{
 			"id": "spring-rolls",
-			"name": "Spring Rolls",
-			"price": "$5",
-			"description": "Description of what's in the dish and how it's cooked.",
+			"name": "Spring Rolls (Quantity: 3 large rolls)",
+			"price": "$6.00",
+			"description": "Fresh veggies, shrimp, and pork wrapped in rice paper alongside with special house sweet fish sauce.",
 			"image": "images/dishes/spring-rolls.jpg",
 			"altText": "spring-rolls"
 		},
 		{
 			"id": "chicken-beef-larb",
 			"name": "Chicken/Beef Larb",
-			"price": "$5",
-			"description": "Description of what's in the dish and how it's cooked.",
+			"price": "$8.00",
+			"description": "Spicy seasoned meat salad with your choice of chicken or beef with a side of fresh veggies.",
 			"image": "images/dishes/chicken-beef-larb.jpg",
 			"altText": "chicken-beef-larb"
 		}
@@ -132,7 +132,7 @@ var menu = {
 		{
 			"id": "boba",
 			"name": "Boba",
-			"price": "$5",
+			"price": "$3.50",
 			"description": "Description of what's in the dish and how it's cooked.",
 			"image": "images/dishes/boba.jpg",
 			"altText": "boba"
--- js/specials.js
+++ js/specials.js
@@ -3,58 +3,58 @@ var specials = {
 		{
 			"id": "ba-baw",
 			"name": "Monday: Ba Baw",
-			"price": "$5",
-			"description": "Description of what's in the dish and how it's cooked.",
+			"price": "$7.00",
+			"description": "Savory Cambodian style rice porridge with combination of chicken, pork, and pork blood.",
 			"image": "images/dishes/ba-baw.jpg",
 			"altText": "ba-baw"
 		},
 		{
 			"id": "salaw-machu-youn",
 			"name": "Tuesday: Salaw Machu Youn",
-			"price": "$5",
-			"description": "Description of what's in the dish and how it's cooked.",
+			"price": "$9.00",
+			"description": "Cambodian sour soup with fish, shrimp, and fresh herbs.",
 			"image": "images/dishes/Salaw-Machu-Youn.jpg",
 			"altText": "Salaw-Machu-Youn"
 		},
 		{
 			"id": "loc-lac",
 			"name": "Wednesday: Loc Lac",
-			"price": "$5",
-			"description": "Description of what's in the dish and how it's cooked.",
+			"price": "$8.00",
+			"description": "Savory sautéed beef over fresh vegetables with side of pepper lime sauce.",
 			"image": "images/dishes/Loc-Lac.jpg",
 			"altText": "loc-lac"
 		},
 		{
-			"id": "Vermicilli-bay-hoy",
-			"name": "Thursday: Vermicilli Bay Hoy",
-			"price": "$5",
-			"description": "Description of what's in the dish and how it's cooked.",
-			"image": "images/dishes/Vermicilli-bay-hoy.jpg",
-			"altText": "Vermicilli-bay-hoy"
+			"id": "Yom-salad",
+			"name": "Thursday: Yom Salad",
+			"price": "$9.00",
+			"description": "Traditional Khmer salad with shrimp, chicken, fresh veggies, vermicelli, and our traditional house sauce.",
+			"image": "images/dishes/Yom-salad.jpg",
+			"altText": "Yom-salad"
 		},
 		{
 			"id": "Ban-Xeo",
-			"name": "Friday: Ban Xeo",
-			"price": "$5",
-			"description": "Description of what's in the dish and how it's cooked.",
+			"name": "Friday: Ban Xeo (Quantity: 2 Crepes)",
+			"price": "$8.00",
+			"description": "Cambodian style sizzling pancake with ground chicken with a side of fresh veggies and special sweet fish sauce.",
 			"image": "images/dishes/Ban-Xeo.jpg",
 			"altText": "Ban-Xeo"
 		},
 		{
-			"id": "chicken-curry",
-			"name": "Saturday: Chicken Curry",
-			"price": "$5",
-			"description": "Description of what's in the dish and how it's cooked.",
+			"id": "curry",
+			"name": "Saturday: Red (Chicken) / Green (Fish) Curry",
+			"price": "$8.00",
+			"description": "Traditional Cambodian curry served with noodles with your choice of red or green type of curry. Red curry is made with chicken, Green curry is made with fish.",
 			"image": "images/dishes/chicken-curry.jpg",
-			"altText": "chicken-curry"
+			"altText": "curry"
 		},
 		{
-			"id": "pahlok-ktiss",
-			"name": "Sunday: Pahlok Ktiss",
-			"price": "$5",
-			"description": "Description of what's in the dish and how it's cooked.",
-			"image": "images/dishes/pahlok-ktiss.jpg",
-			"altText": "pahlok-ktiss"
+			"id": "Beef-Pahok",
+			"name": "Sunday: Beef Pahok",
+			"price": "$8.00",
+			"description": "Tender beef steak cooked medium well and sliced. Served with our spicy traditional Khmer Prahok sauce.",
+			"image": "images/dishes/Beef-Pahok.jpg",
+			"altText": "Beef-Pahok"
 		}
 	]
 }
