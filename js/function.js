bfbe2d194fd76e1f3df18128ea166d99aa2fd5c5
Amit Patel <amitpatel_7@msn.com>
2017-04-29 10:07:38 -0700

    added scrolling and hamburger close functions
    when user clicks on a nav item

--- js/function.js
+++ js/function.js
@@ -1,4 +1,27 @@
-$('.hamburger').on('click', function () {
-	$('.menu').toggleClass('open');
+$(document).ready(function(){
+
+	$('.hamburger').on('click', function () {
+		$('.menu').toggleClass('open');
+	});
+
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').animate({
+	        'scrollTop': $target.offset().top
+	    }, 1000, 'swing', function () {
+	        window.location.hash = target;
+	    });
+
+
+	});
+
+
 });
 
+$(".clearfix li").on('click', function () {
+    $('.menu').toggleClass('open');
+});
